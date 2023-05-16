f05303808e359ae9e514e57b598c19642db14259
JulieBourelDrapier <juliebourel@hotmail.com>
2023-05-16 17:30:12 +0200

    STEP BACK modification of the ID "gallery" witch becomes a class again (because the css properties are accurate when that element as a class)

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,6 +3,7 @@ const images = document.getElementById("gallery");
 
 fetch("http://localhost:5678/api/works/")
   .then(res => {
+    console.log(res);
     if(res.ok){
       res.json().then(data => {
         images.src = data[0].url
