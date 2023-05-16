cee3cf7ad80d09810a72a4ea7a3d087139bd42e3
JulieBourelDrapier <juliebourel@hotmail.com>
2023-05-16 16:48:28 +0200

    if/else conditions ADD to fetch to see when there is a pb ("working" VS "not working")

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,14 @@
-console.log("Hello")
 
-fetch("http://localhost:5678/api/works")
-  .then(res => res.json())
+
+fetch("http://localhost:5678/api/works/")
+  .then(res => {
+    if (res.ok) {
+      console.log("It is working")
+    } else {
+      console.log("It is NOT working")
+    }
+  })
   .then(data => console.log(data))
 
-  //help from https://www.youtube.com/watch?v=cuEtnrL9-H0 
+
+  
