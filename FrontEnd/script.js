7b1819d87ee9919fb97894b6b4e0f03f113c0885
JulieBourelDrapier <juliebourel@hotmail.com>
2023-05-16 17:08:03 +0200

    ADD / ; / + ortho correction

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,9 +1,9 @@
 //javascript asyncrome => fetch => Get request
-const images = document.getElementById("gallery")
+const images = document.getElementById("gallery");
 
 fetch("http://localhost:5678/api/works/")
   .then(res => res.json())
-  .then(data => imgages.src = data[0].url)
+  .then(data => images.src = data[0].url)
 
 
   
