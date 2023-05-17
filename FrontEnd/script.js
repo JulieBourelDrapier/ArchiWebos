8c50ba4e349e8f59abd28cda94c18d7ec7173104
JulieBourelDrapier <juliebourel@hotmail.com>
2023-05-17 19:56:18 +0200

    ADD filters

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -7,6 +7,7 @@ fetch("http://localhost:5678/api/works/")
     if(res.ok){
       res.json().then(data => {
         for (let i = 0; i < data.length; i++) {
+          //générer les élements 
           const figure = document.createElement('figure')
           const img = document.createElement('img')
           const figcaption = document.createElement('figcaption')
@@ -16,16 +17,17 @@ fetch("http://localhost:5678/api/works/")
           img.setAttribute('alt', data[i].title)
           figcaption.innerText = data[i].title
   
-          //générer les élements 
+          // placer les éléments générés
           figure.append(img)
           figure.append(figcaption)
-  
+
+          // ajouter les éléments dans le DOM
           galleryDiv.append(figure)
         }
         
       })
     } else {
-      console.log("NOT working...")
+      console.error('No data received')
     }
   })
   .catch(e => {
@@ -33,8 +35,36 @@ fetch("http://localhost:5678/api/works/")
     console.error('Penser à fr npm start')
   })
 
+  const filters = document.getElementById('filters')
   // création des boutons filtres + fonction filter 
+fetch("http://localhost:5678/api/categories/")
+  .then(res => {
+    if(res.ok){
+      const ulFilters = document.createElement('ul')
+      const liAll = document.createElement('li')
+      
+      liAll.innerText = 'TOUS'
 
+      ulFilters.append(liAll)
+
+      filters.append(ulFilters)
+
+      res.json().then(data => {
+        for (let i = 0; i < data.length; i++) {
+          const li = document.createElement('li')
+          li.innerText = data[i].name
+          ulFilters.append(li)
+          li.classList.add("filter")//lien vers la partie css pour le style je dois créer le style de ma class filter
+        }
+      })
+
+    }else {
+      console.error('No data received')
+    }
+  })
+  .catch(error => {
+    console.error(error)
+  })
  // faire le filter en comparant l'id du filtre selectionné avec le categorieId des data
   
       
