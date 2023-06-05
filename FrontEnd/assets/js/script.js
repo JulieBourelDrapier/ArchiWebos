7a79877b5c54bf7e6f3cea4f5b3b7e9f72712a75
JulieBourelDrapier <juliebourel@hotmail.com>
2023-06-05 16:58:42 +0200

    add functions // modal (script.js) + delete display in html l.76

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -133,3 +133,44 @@ fetch("http://localhost:5678/api/categories/")
   console.error(error)
 })
 
+// en lien avec la modale
+let modal = null
+
+const openModal = function (event) {
+  event.preventDefault()
+  const target = document.querySelector(event.target.getAttribute("href"))
+  target.style.display = null
+  target.removeAttribute("aria-hidden")
+  target.setAttribute("aria-modal", "true")
+  modal = target 
+  modal.addEventListener("click", closeModal)
+  modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
+  modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+}
+
+const closeModal = function (event) {
+  if (modal === null) return
+  event.preventDefault()
+  modal.style.display = "none"
+  modal.setAttribute("aria-hidden" , "true")
+  modal.removeAttribute("aria-modal")
+  modal.removeEventListener("click", closeModal)
+  modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
+  modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+  modal = null
+}
+
+
+const stopPropagation = function (event) {
+  event.stopPropagation()
+}
+
+document.querySelectorAll(".js-modal").forEach(a => {
+  a.addEventListener("click", openModal)
+})
+
+window.addEventListener("keydown", function (event){
+  if (event.key === "escape" || event.key === "Esc") {
+    closeModal(e)
+  }
+})
