1bacd0d9bfd2ccb1485eba49f7f6c6b4ce5133c3
JulieBourelDrapier <juliebourel@hotmail.com>
2023-06-06 11:14:05 +0200

    delete grafikart modal + add new modal

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -134,43 +134,46 @@ fetch("http://localhost:5678/api/categories/")
 })
 
 // en lien avec la modale
-let modal = null
+//let modal = null
 
-const openModal = function (event) {
-  event.preventDefault()
-  const target = document.querySelector(event.target.getAttribute("href"))
-  target.style.display = null
-  target.removeAttribute("aria-hidden")
-  target.setAttribute("aria-modal", "true")
-  modal = target 
-  modal.addEventListener("click", closeModal)
-  modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
-  modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
-}
+//const openModal = function (event) {
+ // event.preventDefault()
+ // const target = document.querySelector(event.target.getAttribute("href"))
+  //target.style.display = null
+  //target.removeAttribute("aria-hidden")
+  //target.setAttribute("aria-modal", "true")
+  //modal = target 
+  //modal.addEventListener("click", closeModal)
+  //modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
+  //modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+//}
 
-const closeModal = function (event) {
-  if (modal === null) return
-  event.preventDefault()
-  modal.style.display = "none"
-  modal.setAttribute("aria-hidden" , "true")
-  modal.removeAttribute("aria-modal")
-  modal.removeEventListener("click", closeModal)
-  modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
-  modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
-  modal = null
-}
+//const closeModal = function (event) {
+  //if (modal === null) return
+  //event.preventDefault()
+  //modal.style.display = "none"
+  //modal.setAttribute("aria-hidden" , "true")
+  //modal.removeAttribute("aria-modal")
+  //modal.removeEventListener("click", closeModal)
+  //modal.querySelector(".js-modal-close").removeEventListener("click", closeModal)
+  //modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation)
+  //modal = null
+//}
 
 
-const stopPropagation = function (event) {
-  event.stopPropagation()
-}
+//const stopPropagation = function (event) {
+  //event.stopPropagation()
+//}
 
-document.querySelectorAll(".js-modal").forEach(a => {
-  a.addEventListener("click", openModal)
-})
+//document.querySelectorAll(".js-modal").forEach(a => {
+  //a.addEventListener("click", openModal)
+//})
 
-window.addEventListener("keydown", function (event){
-  if (event.key === "escape" || event.key === "Esc") {
-    closeModal(e)
-  }
-})
+//window.addEventListener("keydown", function (event){
+  //if (event.key === "escape" || event.key === "Esc") {
+    //closeModal(e)
+ // }
+//})
+
+// revoir la configuration des éléments par rapport à ce que j'ai besoin ici. 
+// créer addEventListener qui permette d'ouvrir la modale quand on clic sur "Modifier" (partie "mes projets")
