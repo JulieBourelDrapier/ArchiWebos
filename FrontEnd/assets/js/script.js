951b8df2ffdbd438e82d8947a7b2e5d4f43fd135
JulieBourelDrapier <juliebourel@hotmail.com>
2023-06-29 10:07:09 +0200

    Delete comments & console.logs

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,17 +1,17 @@
 "use strict";
 
-//************** ma constante ***************
+//************** constantes ***************
 const fileTypes = [
   "image/jpeg",
   "image/jpg",
   "image/png",
 ];
 
-//************* fonctions ****************
+//************* functions ****************
 function showHiddenElements() { 
   const connexionLink = document.querySelector(".connexion-link");
-  let hiddenElements = document.getElementsByClassName("hidden");
-  const token        = localStorage.getItem("token");
+  let hiddenElements  = document.getElementsByClassName("hidden");
+  const token         = localStorage.getItem("token");
   if (token) {
     Array.from(hiddenElements).forEach((element) => {
       element.classList.remove("hidden");
@@ -62,18 +62,18 @@ function createGallery(selector, categoryId = null, result = null) {
     data = result;
   }
   for (let i = 0; i < data.length; i++) {
-    //générer les élements 
+    
     const figure     = document.createElement('figure');
     const img        = document.createElement('img');
     const figcaption = document.createElement('figcaption');
-    //configurer
+ 
     img.setAttribute('src', data[i].imageUrl);
     img.setAttribute('alt', data[i].title);
     figcaption.innerText = data[i].title;
-    // placer les éléments générés
+    
     figure.append(img);
     figure.append(figcaption);
-    // ajouter les éléments dans le DOM
+
     galleryDiv.append(figure);
   }
 }
@@ -83,48 +83,48 @@ function createGalleryModal(data = null) {
   galleryDiv.innerHTML = "";
 
   for (let i = 0; i < data.length; i++) {
-    //générer les élements 
+    
     const figure         = document.createElement('figure');
     const img            = document.createElement('img');
     const iconsContainer = document.createElement('div');
     const figcaption     = document.createElement('figcaption');
     const iconVisible    = document.createElement('i');
     const iconInvisible  = document.createElement('i');
-    //configurer
+   
     img.setAttribute('src', data[i].imageUrl);
     img.setAttribute('alt', data[i].title);
     figcaption.innerText = "éditer";
     iconsContainer.classList.add("icons-container");
     iconVisible.classList.add("fa", "fa-trash-can","icons", "icon1");
     iconInvisible.classList.add("fa-solid", "fa-arrows-up-down-left-right", "icons", "icon2");
-    //setting of  display property for icons
-      iconVisible.style.display   = "inline-block";
-      iconInvisible.style.display = "inline-block";
-      iconInvisible.style.opacity = "0";
-    //addEventListener to make the icon appear and disappear
-      figure.addEventListener('mouseenter', () => {
-      iconInvisible.style.opacity = "1";
-      figure.style.transform      = "scale(1.2)";
-    })
-      figure.addEventListener('mouseleave', () => {
-      iconInvisible.style.opacity = "0";
-      figure.style.transform      = "scale(1)";
-    })
-    // placer les éléments générés
-      figure.append(img);
-      figure.append(figcaption);
-      figure.append(iconsContainer);
-      iconsContainer.append(iconVisible);
-      iconsContainer.append(iconInvisible);
-    // ajouter les éléments dans le DOM
-      galleryDiv.append(figure);
     
-      iconVisible.addEventListener("click", deletePhoto(data[i].id, figure));
+    iconVisible.style.display   = "inline-block";
+    iconInvisible.style.display = "inline-block";
+    iconInvisible.style.opacity = "0";
+
+    figure.addEventListener('mouseenter', () => {
+    iconInvisible.style.opacity = "1";
+    figure.style.transform      = "scale(1.2)";
+    })
+
+    figure.addEventListener('mouseleave', () => {
+    iconInvisible.style.opacity = "0";
+    figure.style.transform      = "scale(1)";
+    })
+    
+    figure.append(img);
+    figure.append(figcaption);
+    figure.append(iconsContainer);
+    iconsContainer.append(iconVisible);
+    iconsContainer.append(iconInvisible);
+    
+    galleryDiv.append(figure);
+    
+    iconVisible.addEventListener("click", deletePhoto(data[i].id, figure));
   }
 }
 
 function closeModal (e) {
-  console.log("close modal")
   const modal = document.querySelector(".modal");
   modal.remove();
   document.removeEventListener("click", closeModal);
@@ -155,14 +155,14 @@ async function createCategoriesOptions() {
 }
 
 async function createSecondModal (e) {
-  //récup les éléments existants 
+  
   const secondModalAside     = document.createElement("aside");
   const secondModalDiv       = document.createElement("div");
   const divNav               = document.createElement("div");
   const leftArrow            = document.createElement("i");
   const secondModalFirstBtn  = document.createElement("button");
   const secondModalTitle     = document.createElement("h3"); 
-  //ELEMENTS DU FORMULAIRE
+ 
   const form                 = document.createElement("form");
   const applySelectionDiv    = document.createElement("div");
   const landscapeIcon        = document.createElement("i");
@@ -176,44 +176,44 @@ async function createSecondModal (e) {
   const borderDiv            = document.createElement("div");
   const submitBtn            = document.createElement("button");
   const previewDiv           = document.createElement("div");
-  //configurer
-  secondModalAside.id               = "modal2";
-  secondModalAside.classList.add    ("modal");
-  secondModalDiv.classList.add      ("modal-wrapper", "js-modal-stop");
-  divNav.classList.add              ("div-nav");
-  leftArrow.classList.add           ("fa-sharp", "fa-solid", "fa-arrow-left", "icon3");
-  secondModalFirstBtn.classList.add ("js-second-modal-close");
-  secondModalFirstBtn.innerText     = "x";
-  secondModalTitle.classList.add    ("title-modal");
-  secondModalTitle.innerText        = "Ajout photo";
-  //CONFIG DES ELEMENTS EN LIEN AVEC LE FORMULAIRE
-  form.classList.add                ("second-modal-form");
-  applySelectionDiv.classList.add   ("apply-selection-div");
-  landscapeIcon.classList.add       ("fa-regular", "fa-image", "icon4");
-  addImageInput.type                = "file";
-  addImageInput.id                  = "image-input";
-  addImageInput.style.opacity       = "0";
-  addImageInput.accept              = ".jpg, .jpeg, .png";
-  addImageLabel.textContent         = "+ Ajouter photo";
-  addImageLabel.htmlFor             = "image-input";
-  addImageLabel.id                  = "js-second-modal-add-photo";
-  suggSpan.id                       = "suggestions-span";
-  suggSpan.innerText                = "jpg, png : 4mo max";
-  titleLabel.id                     = "title-label";
-  titleLabel.textContent            = "Titre";
-  titleInput.id                     = "title-input";
-  titleInput.type                   = "text";
-  titleInput.name                   = "title";
-  titleInput.required               = true;
-  categorySelect.id                 = "category-select";
-  categoryLabel.textContent         = "Catégorie";
-  borderDiv.id                      = "border-div"; 
-  submitBtn.classList.add           ("submit-btn");
-  submitBtn.id                      = "validate-btn";
-  submitBtn.type                    = "submit";
-  submitBtn.textContent             = "Valider";
+  
+  secondModalAside.id = "modal2";
+  secondModalAside.classList.add("modal");
+  secondModalDiv.classList.add("modal-wrapper", "js-modal-stop");
+  divNav.classList.add("div-nav");
+  leftArrow.classList.add("fa-sharp", "fa-solid", "fa-arrow-left", "icon3");
+  secondModalFirstBtn.classList.add("js-second-modal-close");
+  secondModalFirstBtn.innerText = "x";
+  secondModalTitle.classList.add("title-modal");
+  secondModalTitle.innerText = "Ajout photo";
+  
+  form.classList.add("second-modal-form");
+  applySelectionDiv.classList.add("apply-selection-div");
+  landscapeIcon.classList.add("fa-regular", "fa-image", "icon4");
+  addImageInput.type = "file";
+  addImageInput.id = "image-input";
+  addImageInput.style.opacity = "0";
+  addImageInput.accept = ".jpg, .jpeg, .png";
+  addImageLabel.textContent = "+ Ajouter photo";
+  addImageLabel.htmlFor = "image-input";
+  addImageLabel.id = "js-second-modal-add-photo";
+  suggSpan.id = "suggestions-span";
+  suggSpan.innerText = "jpg, png : 4mo max";
+  titleLabel.id = "title-label";
+  titleLabel.textContent = "Titre";
+  titleInput.id = "title-input";
+  titleInput.type = "text";
+  titleInput.name = "title";
+  titleInput.required = true;
+  categorySelect.id = "category-select";
+  categoryLabel.textContent = "Catégorie";
+  borderDiv.id = "border-div"; 
+  submitBtn.classList.add("submit-btn");
+  submitBtn.id = "validate-btn";
+  submitBtn.type = "submit";
+  submitBtn.textContent = "Valider";
   previewDiv.classList.add("preview")
-  //placer dans le dom
+  
   document.body.prepend(secondModalAside);
   secondModalAside.append(secondModalDiv);
   secondModalDiv.append(divNav, secondModalTitle, applySelectionDiv, form);
@@ -225,11 +225,11 @@ async function createSecondModal (e) {
   })
   form.append(applySelectionDiv, titleLabel, titleInput, categoryLabel, categorySelect, borderDiv, submitBtn);
 
-
   leftArrow.addEventListener("click", () => {
     closeModal()
     createModal()
   });
+
   secondModalDiv.addEventListener("click", stopPropagation);
   window.addEventListener('click', clickOutsideModal, {capture: true});
   secondModalFirstBtn.addEventListener("click", closeModal);
@@ -249,10 +249,10 @@ async function createSecondModal (e) {
 }
 
 function formValidation(e) {
-  const titleInput = document.getElementById("title-input");
+  const titleInput     = document.getElementById("title-input");
   const categorySelect = document.getElementById("category-select");
-  const addImageInput = document.getElementById("image-input");
-  const submitBtn = document.getElementById("validate-btn");
+  const addImageInput  = document.getElementById("image-input");
+  const submitBtn      = document.getElementById("validate-btn");
 
   if (titleInput.value === "" || categorySelect.value === "" || addImageInput.files.length === 0) {
     submitBtn.classList.remove("validated")
@@ -270,7 +270,7 @@ async function createModal (e) {
   const modalSecondBtn = document.createElement("button");
   const modalDelete    = document.createElement("a");
   const modalGallery   = document.createElement("div");
-  //configurer
+ 
   modalAside.id = "modal1";
   modalAside.classList.add("modal");
   modalDiv.classList.add("modal-wrapper", "js-modal-stop");
@@ -285,7 +285,7 @@ async function createModal (e) {
   modalDelete.classList.add("js-delete-gallery");
   modalDelete.innerText = "Supprimer la galerie";
   modalDelete.href="#";
-  //placer dans le dom
+  
   document.body.prepend(modalAside);
   modalAside.append(modalDiv);
   modalDiv.append(modalFirstBtn, modalTitle, modalGallery, modalSecondBtn, modalDelete);
@@ -295,14 +295,14 @@ async function createModal (e) {
   modalFirstBtn.addEventListener("click", closeModal);
   modalAside.addEventListener("click", stopPropagation);
   window.addEventListener('click', clickOutsideModal, {capture: true});
-  // window.addEventListener('click', clickOutsideModal);
   modalSecondBtn.addEventListener("click", createSecondModal);
 }
 
 function updateImageDisplay(e) {
-  const preview = document.querySelector('.preview')
-  const input = document.querySelector('#image-input')
+  const preview       = document.querySelector('.preview')
+  const input         = document.querySelector('#image-input')
   const landscapeIcon = document.querySelector(".fa-regular.fa-image.icon4")
+
   while (preview.firstChild) {
     preview.removeChild(preview.firstChild);
   }
@@ -355,7 +355,6 @@ function returnFileSize(number) {
 }
 
 function clickOutsideModal(e) {
-  console.log(e.target)
   if (!document.querySelector(".modal-wrapper.js-modal-stop")?.contains(e.target)) {
     closeModal();
     window.removeEventListener('click', clickOutsideModal, {capture: true})
@@ -363,7 +362,6 @@ function clickOutsideModal(e) {
 }
 
 function stopPropagation (e) {
-  console.log("stopPropagation");	
   e.stopPropagation();
 }
 
@@ -371,7 +369,6 @@ function deletePhoto(id, img) {
   return async (e) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log("Suppression en cours...");
     try {
       const response = await fetch ("http://localhost:5678/api/works/" + id, {
         method: "DELETE",
@@ -425,24 +422,22 @@ async function submitPhoto (e) {
   });
 }
 
-  
-//************** MAIN CODE ***************
 
 function initFilters() {
   fetch("http://localhost:5678/api/categories/")
   .then(res => {
     if(res.ok){
       const ulFilters = document.createElement('ul');
-      const liAll = document.createElement('li');
-      const filters = document.getElementById('filters');
-      //configuer 
+      const liAll     = document.createElement('li');
+      const filters   = document.getElementById('filters');
+      
       liAll.innerText = "Tous";
       liAll.addEventListener("click", filterImgEvent);
       liAll.classList.add("filter", "selected");
       ulFilters.classList.add("filterParent");
-      //placer
+      
       ulFilters.append(liAll);
-      //ajouter dans le DOM
+      
       filters.append(ulFilters);
   
       res.json().then(data => {
@@ -473,5 +468,7 @@ async function init() {
   showHiddenElements();
   initFilters()
 }
+
+//************** main ***************
 
 init()
